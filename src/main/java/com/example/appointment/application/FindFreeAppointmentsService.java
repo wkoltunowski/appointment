b4120e58bdd581c8d24f7cd51c5071697b1d6410
Wojciech Koltunowski <wkoltunowski@gmail.com>
2016-04-10 21:52:41 +0200

    return free appointments date ordered

--- src/main/java/com/example/appointment/application/FindFreeAppointmentsService.java
+++ src/main/java/com/example/appointment/application/FindFreeAppointmentsService.java
@@ -1,17 +1,14 @@
 package com.example.appointment.application;
 
-import com.example.appointment.domain.freeslot.FreeAppointment;
-import com.example.appointment.domain.freeslot.FreeAppointments;
-import com.example.appointment.domain.freeslot.FreeSlot;
-import com.example.appointment.domain.freeslot.FreeSlotRepository;
+import com.example.appointment.domain.freeslot.*;
 import com.example.appointment.domain.schedule.ScheduleDurations;
-import com.example.appointment.domain.freeslot.SearchTags;
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.Multimap;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Spliterator;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -36,17 +33,36 @@ public class FindFreeAppointmentsService {
 
     public FreeAppointments findFirstFree(LocalDateTime startingFrom, SearchTags searchTags) {
         LocalDate startingDay = startingFrom.toLocalDate();
-
-
-
-        List<FreeAppointment> freeAppointments = StreamSupport
+        Iterator<FreeSlot> iterator = StreamSupport
                 .stream(this.storage.findAfter(startingDay).spliterator(), false)
                 .filter(fs -> fs.matches(searchTags))
-                .flatMap(appointmentsStream(startingFrom))
-                .limit(firstFreeCount)
-                .collect(toList());
+                .iterator();
+        Multimap<LocalDate, FreeSlot> map = LinkedListMultimap.create();
+        List<FreeAppointment> freeAppointments = new ArrayList<>(firstFreeCount);
+        while (iterator.hasNext() && freeAppointments.size() < firstFreeCount) {
+            FreeSlot freeSlot = iterator.next();
+            LocalDate slotDay = freeSlot.start().toLocalDate();
+            if (!map.containsKey(slotDay)) {
+                addToFreeAppointments(freeAppointments, startingFrom, map.values());
+                map.clear();
+            }
+            map.put(slotDay, freeSlot);
+        }
+
+        addToFreeAppointments(freeAppointments, startingFrom, map.values());
+
         return FreeAppointments.of(freeAppointments);
     }
+
+    private void addToFreeAppointments(List<FreeAppointment> freeAppointments, LocalDateTime startingFrom, Collection<FreeSlot> values) {
+        freeAppointments.addAll(values
+                .stream()
+                .flatMap(appointmentsStream(startingFrom))
+                .sorted(Comparator.comparing(FreeAppointment::start).thenComparing(a -> Duration.between(a.start(), a.end())))
+                .limit(firstFreeCount - freeAppointments.size())
+                .collect(toList()));
+    }
+
 
     private Function<FreeSlot, Stream<FreeAppointment>> appointmentsStream(LocalDateTime startingDate) {
         return fs -> {
